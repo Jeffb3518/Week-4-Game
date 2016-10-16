26f9be3da74b13945381855ce7f1227795481a24
Jeffrey Bauer <Jeffrey@Jeffreys-Air.attlocal.net>
2016-10-16 13:48:34 -0400

    jQuery Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,92 +1,144 @@
 $(document).ready(function() {
 
-var ranNum;
+var winningNumber;
+var score = 0;
 var win = 0;
-var loss= 0;
-var score = ""; 
-var green;
-var yellow;
-var red;
-var blue;
+var loss = 0;
+
+$('#win').text(wins);
+$('#loss').text(loss);
+
+$('.start').text(winningNumber);
+    for (var i=0; i< numbers.length; i++){
+
+      var imageCrystal = $('<img>');
+      imageCrystal.attr('data-num', numbers[i]);
+      imageCrystal.attr('src',);
+      imageCrystal.attr('alt', 'crystals');
+      imageCrystal.addClass('crystalImage');
+      $('#crystals').append(imageCrystal);
+    }
 
 
 
+  $('#crystalImage').on('click', function(){
+		score = score + parseInt($(this).data('num'));
+		   
+		    $('#score').text(counter);
 
-function gameStart() {
-
-	ranNum =  Math.floor(Math.random() * 56) + 17; ;
-	green = Math.floor(Math.random() * 11) + 1 ;
-	yellow = Math.floor(Math.random() * 11) + 1 ;
-	red = Math.floor(Math.random() * 11) + 1 ;
-	blue = Math.floor(Math.random() * 11) + 1 ;
-
-	score = 0;
-	$('#score').html(score);
-	$('.start').html(ranNum);			
-}
-
-gameStart();
-
-
-$('#win').html(win);
-$('#loss').html(loss);
-	
-
-	$(".green").on("click", function(){					 
-		
-		score += green
-		$('#score').html(score);
-		decider();
-
-
-	});
-
-	$(".yellow").on("click", function(){					
-
-		score += yellow
-		$('#score').html(score);
-		decider();
-
-
-	});
-
-	$(".red").on("click", function(){					
-
-		score += red
-		$('#score').html(score);
-		decider();
-
-
-	});
-
-	$(".blue").on("click", function(){					
-
-		score += blue
-		$('#score').html(score);
-		decider();
-
-	});
+		    if (counter == winningNumber){
+		      alert ('You Won!');
+		      wins ++;
+		      $('#win').text(win);
+		      console.log(wins)
+		      $('#crystals').empty();
+		     
+		        
+		    } else if ( counter > numberToGuess){
+		        alert ('You Lost!');
+		        loss ++;
+		        $('#loss').text(loss);
+		        console.log(loss)
+		        $('#crystals').empty();
+		    }
+		});
+	}
 
 });
 
-console.log(ranNum);
-console.log(score);
 
-function decider(){
 
-	if(score > ranNum) {
 
-		loss++
-		$('#loss').html(loss);
-		gameStart();
 
-	}
 
-	else if(score == ranNum) {
 
-		win++
-		$('#win').html(win)
-		gameStart();
-	}
+// var ranNum;
+// var win = 0;
+// var loss= 0;
+// var score = ""; 
+// var green;
+// var yellow;
+// var red;
+// var blue;
 
-}
+
+
+
+// function gameStart() {
+
+// 	ranNum =  Math.floor(Math.random() * 56) + 17; ;
+// 	green = Math.floor(Math.random() * 11) + 1 ;
+// 	yellow = Math.floor(Math.random() * 11) + 1 ;
+// 	red = Math.floor(Math.random() * 11) + 1 ;
+// 	blue = Math.floor(Math.random() * 11) + 1 ;
+
+// 	score = 0;
+// 	$('#score').html(score);
+// 	$('.start').html(ranNum);			
+// }
+
+// gameStart();
+
+
+// $('#win').html(win);
+// $('#loss').html(loss);
+	
+
+// 	$(".green").on("click", function(){					 
+		
+// 		score += green
+// 		$('#score').html(score);
+// 		decider();
+
+
+// 	});
+
+// 	$(".yellow").on("click", function(){					
+
+// 		score += yellow
+// 		$('#score').html(score);
+// 		decider();
+
+
+// 	});
+
+// 	$(".red").on("click", function(){					
+
+// 		score += red
+// 		$('#score').html(score);
+// 		decider();
+
+
+// 	});
+
+// 	$(".blue").on("click", function(){					
+
+// 		score += blue
+// 		$('#score').html(score);
+// 		decider();
+
+// 	});
+
+// });
+
+// console.log(ranNum);
+// console.log(score);
+
+// function decider(){
+
+// 	if(score > ranNum) {
+
+// 		loss++
+// 		$('#loss').html(loss);
+// 		gameStart();
+
+// 	}
+
+// 	else if(score == ranNum) {
+
+// 		win++
+// 		$('#win').html(win)
+// 		gameStart();
+// 	}
+
+// }
